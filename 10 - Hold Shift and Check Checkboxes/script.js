111952f16b5e630378a2652ae5f3b3f93f530fad
Robert Buchberger <rbuchberger@gmail.com>
2018-03-20 13:24:27 +0100

    10 shift detection works

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,6 +1,23 @@
 // Grab all the checkboxes 
 const checkboxes = document.querySelectorAll('input[type=checkbox]');
-// Attach event listeners for shift (keycode 16)
+
+// Attach event listeners 
+document.addEventListener('keydown', shiftCheck);
+document.addEventListener('keyup', shiftCheck);
+
 // Event function:
 // Check if there is another active checkbox
 // Set all appropriate ones to active
+
+let shiftPressed = false;
+
+function shiftCheck(e) {
+  // Return if it's not a shift key:
+  if (e.key != 'Shift') return;
+  
+  if ( e.type === 'keydown' ) {
+    shiftPressed = true;
+  } else if (e.type === 'keyup') {
+    shiftPressed = false;
+  }
+}
