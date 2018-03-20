473800b397a2f33f61c788f0e6b5f797a16ec59d
Robert Buchberger <rbuchberger@gmail.com>
2018-03-20 13:11:18 +0100

    10 trying it on my own first. pseudocode written

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,6 @@
+// Grab all the checkboxes 
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+// Attach event listeners for shift (keycode 16)
+// Event function:
+// Check if there is another active checkbox
+// Set all appropriate ones to active
