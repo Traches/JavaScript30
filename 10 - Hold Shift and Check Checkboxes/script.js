9ac8a2335466fb4bb7a8ed72e673726266febfdf
Robert Buchberger <rbuchberger@gmail.com>
2018-03-20 15:39:11 +0100

    Switched to click event and built-in shift detect.
    After watching the video, wes did it quite a bit differently than I did. The biggest difference in behavior is that mine is based on the top and bottom checked boxes, while his is based on the most and second most recently checked boxes. His will break if you uncheck the last checked box before shift+clicking the next one, but mine won't handle more complicated selections like his will.

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -5,41 +5,21 @@
 // unchecking, or situations when more than one box is checked. Must work in
 // both directions; bottom to top and top to bottom.  
 
-// Prepare variables:
-let shiftPressed = false;
 
 // Grab all the checkboxes:
 const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
-// Attach event listeners:
-document.addEventListener('keydown', shiftCheck);
-document.addEventListener('keyup', shiftCheck);
-
 checkboxes.forEach(c => {
-  c.addEventListener('change', multiSelect)
+  c.addEventListener('click', multiSelect)
 });
-
-function shiftCheck(e) {
-  // Return if it's not a shift key:
-  if (e.key != 'Shift') return;
-  
-  if (e.type === 'keydown') {
-    shiftPressed = true;
-  } else if (e.type === 'keyup') {
-    shiftPressed = false;
-  }
-}
 
 function multiSelect(e) {
 
   // don't do anything if shift isn't pressed:
-  if (!shiftPressed) return;
+  if (!e.shiftKey) return;
 
   // convert the node module to an array:
   const cbArray = [...checkboxes];
-
-  // Return if there are less than 2 boxes checked:
-  if  (cbArray.filter(c => c.checked).length < 2) return;
 
   // Find the first one, which is pretty easy: 
   const startChecks = cbArray.findIndex(c => c.checked);
