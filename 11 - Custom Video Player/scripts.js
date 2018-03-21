7360d3c6ae5a73f7bfefdada7ca07a7a7147ff4d
Robert Buchberger <rbuchberger@gmail.com>
2018-03-21 11:21:20 +0100

    11 added working fullscreen button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,11 +1,12 @@
 // Get elements
-const player      = document.querySelector('.player');
-const video       = player.querySelector('.viewer');
-const progress    = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle      = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges      = player.querySelectorAll('.player__slider');
+const player           = document.querySelector('.player');
+const video            = player.querySelector('.viewer');
+const progress         = player.querySelector('.progress');
+const progressBar      = player.querySelector('.progress__filled');
+const toggle           = player.querySelector('.toggle');
+const skipButtons      = player.querySelectorAll('[data-skip]');
+const fullScreenButton = player.querySelector('.fullscreen')
+const ranges           = player.querySelectorAll('.player__slider');
 
 // Write functions
 function togglePlay() {
@@ -42,12 +43,21 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  if(document.webkitFullscreenElement) {
+    document.webkitExitFullscreen();
+  } else {
+    player.webkitRequestFullscreen();
+  }
+}
+
 // Event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', progressUpdate);
 toggle.addEventListener('click', togglePlay);
+fullScreenButton.addEventListener('click', toggleFullscreen);
 skipButtons.forEach(b => b.addEventListener('click', skip));
 ranges.forEach(r => r.addEventListener('change', rangeUpdate));
 
