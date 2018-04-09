92130240cb139b2b59942ca9b7530169ee7eb99e
Robert Buchberger <rbuchberger@gmail.com>
2018-04-09 13:43:37 +0200

    13 complete. Wes is cool, but his 'active image' logic here seemed pretty convoluted so I did it differently. I just figured out window and image top & bottom, added an 'offset' constant for tweaking convenience, and set images to display when inside the screen boundaries by the offset amount. I also found that setting 'immediate' to false in the debounce function gave better results; when 'true' you'd get images not sliding in when scrolling fast.

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -1,4 +1,7 @@
-function debounce(func, wait = 20, immediate = true) {
+const slideImages = document.querySelectorAll('.slide-in');
+window.addEventListener('scroll', debounce(checkSlide));
+
+function debounce(func, wait = 20, immediate = false) {
   var timeout;
   return function() {
     var context = this, args = arguments;
@@ -12,3 +15,29 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+function checkSlide(e) {
+  console.count('checkSlide called');
+  const windowTop      = window.scrollY;
+  const windowBot      = windowTop + window.innerHeight;
+  // boundaryOffset is how far past the window's border images should activate,
+  // in pixels:  
+  const boundaryOffset = 100;
+
+  slideImages.forEach(image => {
+    const imageTop    = image.offsetTop
+    const imageBot    = imageTop + image.height
+    const activeImage =
+      // image top is above window bottom by boundaryOffset px:
+      imageTop < windowBot - boundaryOffset &&
+      // and image bottom is below window top by boundaryOffset px:
+      imageBot > windowTop + boundaryOffset;
+
+    if (activeImage) { 
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
