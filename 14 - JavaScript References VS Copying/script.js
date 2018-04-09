3b0ecc97d5ab33a98f1092d615de835b77333d47
Robert Buchberger <rbuchberger@gmail.com>
2018-04-09 19:12:47 +0200

    14 complete

--- 14 - JavaScript References VS Copying/script.js
+++ 14 - JavaScript References VS Copying/script.js
@@ -1,44 +1,106 @@
+// start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
 
-    // start with strings, numbers and booleans
+age = 200;
 
-    // Let's say we have an array
-    const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+console.log(age, age2);
+// 100 200
+// Copies, doesn't reference. 
 
-    // and we want to make a copy of it.
+let name = 'bobert'
+let name2 = name
 
-    // You might think we can just do something like this:
+name = 'bob';
+console.log(name, name2);
+// bob bobert
 
-    // however what happens when we update that array?
+// Let's say we have an array
+const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team = players;
+team[3] = 'Robert';
 
-    // now here is the problem!
+console.log("const team = players:")
+console.log(players, team);
+// 'Ryan' has changed to 'Robert' in both arrays. The second const statement
+// simply created a reference to the original array, not a copy. 
 
-    // oh no - we have edited the original array too!
+// So, how do we fix this? We take a copy instead!
+// slice() with no arguments returns a copy of the array
+const players2 = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team2 = players2.slice() 
+team2[3] = 'Robert';
 
-    // Why? It's because that is an array reference, not an array copy. They both point to the same array!
+console.log("const team2 = players2.splice()")
+console.log(players2, team2);
 
-    // So, how do we fix this? We take a copy instead!
+// or create a new array and concat the old one in
+const players3 = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team3 = [].concat(players3);
 
-    // one way
+// or use the new ES6 Spread
+const players4 = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team4 = [...players4];
 
-    // or create a new array and concat the old one in
+// Array.from() works too
+const players5 = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+const team5 = Array.from(players5);
 
-    // or use the new ES6 Spread
+// The same thing goes for objects, let's say we have a person object
 
-    // now when we update it, the original one isn't changed
+// with Objects
+const person = {
+  name: 'Wes Bos',
+  age: 80
+};
 
-    // The same thing goes for objects, let's say we have a person object
+// and think we make a copy:
+const personCopy = person
+personCopy.age = 100;
 
-    // with Objects
-    const person = {
-      name: 'Wes Bos',
-      age: 80
-    };
+console.log('const personCopy = person');
+console.log(person.age, personCopy.age);
+// 100 100
 
-    // and think we make a copy:
+// how do we take a copy instead?
 
-    // how do we take a copy instead?
+const person2 = {
+  name: 'Wes Bos',
+  age: 80
+};
 
-    // We will hopefully soon see the object ...spread
+const personCopy2 = Object.assign({}, person2, {foo: 'bar', age: 100});
+console.log('Object.assign()');
+console.log(person2.age, personCopy2.age);
+// 80 100
 
-    // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
 
+object1 = {
+  name: 'Bob',
+  numbers: [1,2,3,4]
+}
+
+object2 = Object.assign({}, object1);
+object2.numbers.splice(2,1);
+
+console.log('Nested array in an object duplicated with Object.assign()')
+console.log(object1.numbers, object2.numbers);
+// [1,2,4] [1,2,4]
+
+// You rarely actually need to do this. But if you do, the thing to google is
+// "deep clone"
+
+// Poor man's deep clone: 
+object3 = {
+  name: 'Bob',
+  numbers: [1,2,3,4]
+}
+
+object4 = JSON.parse(JSON.stringify(object3));
+
+object4.numbers.splice(2,1)
+console.log('b = JSON.parse(JSON.stringify(a))')
+console.log('I need a shower...');
+console.log(object3.numbers, object4.numbers);
+// [1,2,3,4] [1,2,4]
