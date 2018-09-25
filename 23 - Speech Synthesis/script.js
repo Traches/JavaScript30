4ad3c5c9b0e9dde0f7c910bb6dddf60b5d87adf9
Robert Buchberger <rbuchberger@gmail.com>
2018-09-25 12:22:49 +0200

    23 complete

--- 23 - Speech Synthesis/script.js
+++ 23 - Speech Synthesis/script.js
@@ -1,8 +1,38 @@
 /* eslint no-unused-vars: off */
 /* eslint no-console: off */
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
+const voicesDropdown = document.querySelector('[name = "voice"]');
+const options = document.querySelectorAll('[type = "range"], [name  = "text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+const msg = new SpeechSynthesisUtterance();
+msg.text = document.querySelector('[name = "text"]').value;
+
+let voices = [];
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en'))
+    .map( voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>` )
+    .join('');
+}
+
+voicesDropdown.addEventListener('change', setVoice);
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+}
+
+options.forEach(option => option.addEventListener('change', setOption));
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if(startOver) { speechSynthesis.speak(msg); }
+}
