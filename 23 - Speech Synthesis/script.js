9fb740cbf6e1cd32b491954f5c6950ec8f2da8ed
Robert Buchberger <rbuchberger@gmail.com>
2018-09-25 11:15:44 +0200

    23 Setup

---
+++ 23 - Speech Synthesis/script.js
@@ -0,0 +1,8 @@
+/* eslint no-unused-vars: off */
+/* eslint no-console: off */
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
