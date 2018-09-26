0efa85f6ad3c0c469f93bf9628233005b074439a
Robert Buchberger <rbuchberger@gmail.com>
2018-09-26 12:36:55 +0200

    25 complete

--- 25 - Event Capture, Propagation, Bubbling and Once/script.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -1,1 +1,23 @@
 /* eslint no-console: off, no-unused-vars: off */
+
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+button.addEventListener('click', buttonClick, {
+  capture: false,
+  once: true
+});
+
+function buttonClick(e) {
+  console.log('button!');
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: false,
+  once: true
+}));
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation();
+}
