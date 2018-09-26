2a697d4626740ed5b160d397caf47076bc30cf21
Robert Buchberger <rbuchberger@gmail.com>
2018-09-26 12:20:42 +0200

    24 complete

--- 24 - Sticky Nav/script.js
+++ 24 - Sticky Nav/script.js
@@ -1,1 +1,16 @@
 /* eslint no-console: off, no-unused-vars: off  */
+
+const nav = document.querySelector('#main');
+
+window.addEventListener('scroll', fixNav);
+const topOfNav = nav.offsetTop;
+function fixNav() {
+  if(topOfNav <= window.scrollY) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
